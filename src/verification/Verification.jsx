5eb588ffd388dbe2e5d7e91439b44f87ee5f0dcb
Mohd Shazil Raza <shazilraza8@gmail.com>
2025-07-24 04:30:52 +0530

    Fix#04

--- src/verification/Verification.jsx
+++ src/verification/Verification.jsx
@@ -1,10 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import authService from "../../services/Auth";
 import { toast } from "react-toastify";
-import { useNavigate } from "react-router-dom";
-import { runAuthWatcher } from "../../other/AuthWatcher";
+import authService from "../../services/Auth";
 const Verification = () => {
-  const navigate = useNavigate();
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
     const userId = params.get("userId");
@@ -16,11 +14,12 @@ const Verification = () => {
       .verifyEmailAddress(userId, secret)
       .then(function (response) {
         toast.success("Verified");
-        navigate("/home");
       })
       .catch(function (error) {
         toast.error("Not verified!");
-        navigate("/login");
+      })
+      .finally(() => {
+        authService.logout();
       });
   }, []);
   return <div>verifying..</div>;
