e297ab8800fdaceae923c707cbdebc18f28360d6
Mohd Shazil Raza <shazilraza8@gmail.com>
2025-08-21 12:57:47 +0530

    some ux enhacement

--- src/verification/Verification.jsx
+++ src/verification/Verification.jsx
@@ -6,8 +6,6 @@ import { useNavigate } from "react-router-dom";
 import { Check } from "lucide-react";
 const Verification = () => {
   const navigate = useNavigate();
-  const [verified, setVerified] = useState(false);
-  const [verifying, setVerifying] = useState(true);
 
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
@@ -16,7 +14,6 @@ const Verification = () => {
 
     if (!userId || !secret) {
       toast.error("Invalid Link");
-      setVerified(false);
       setTimeout(() => navigate("/auth"), 2000);
       return;
     }
@@ -24,42 +21,24 @@ const Verification = () => {
     authService
       .verifyEmailAddress(userId, secret)
       .then(() => {
-        setVerifying(false);
-        setVerified(true);
-        setTimeout(() => navigate("/home"), 2000);
+        toast.success("Verified ✅ you can log in now.");
       })
       .catch(() => {
-        setVerifying(false);
-        setVerified(false);
-        setTimeout(() => navigate("/auth"), 2000);
+        toast.error("Verification failed ❌");
+      })
+      .finally(() => {
+        authService.logout();
+        navigate("/auth");
       });
   }, []);
 
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center bg-[var(--brand-color)] fixed top-0 left-0">
-      {!verifying ? (
-        verified === true ? (
-          <div className="flex flex-col items-center gap-2 text-white">
-            <p>✅ Verified</p>
-            <Check color="white" />
-          </div>
-        ) : (
-          <div className="flex flex-col items-center gap-2 text-white">
-            <p>❌ Verification Failed</p>
-          </div>
-        )
-      ) : (
-        <>
-          <ThreeDot
-            color={["#cccccc", "#e6e6e6", "#ffffff", "#ffffff"]}
-            text="Please wait, verifying you..."
-            textColor="white"
-          />
-          <p className="font-bold text-xs md:text-md tracking-wider uppercase">
-            Please wait, verifying you...
-          </p>
-        </>
-      )}
+      <ThreeDot
+        color={["#cccccc", "#e6e6e6", "#ffffff", "#ffffff"]}
+        text="Please wait, verifying you..."
+        textColor="white"
+      />
     </div>
   );
 };
